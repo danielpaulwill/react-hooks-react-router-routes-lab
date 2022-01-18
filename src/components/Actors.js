0bce1493ae2e22831274eab7c745403a50cff242
Daniel Williams <danielpaulwill@gmail.com>
2022-01-17 20:25:34 -0500

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,26 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+
+  const actorsList = actors.map((actor) => (
+    <div>
+      <h2>Name: {actor.name}</h2>
+      Movies: 
+      <ul key={actor}>
+        {actor.movies.map((movie) => {
+          return (
+          <li key={actor.name.length.toString() + movie}>{movie}</li>
+        )})}
+      </ul>
+    </div>
+  ))
+
+  return (
+    <div>
+      <h1>Actors Page</h1>
+      {actorsList}
+    </div>
+  );
 }
 
 export default Actors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,26 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+
+  const movieList = movies.map((movie) => (
+    <div>
+      <h2>Title: {movie.title}</h2>
+      <p>Time: {movie.time}</p>
+      Genres: 
+      <ul key={movie}>
+        {movie.genres.map((genre) => (
+          <li key={genre}>{genre}</li>
+        ))}
+      </ul>
+    </div>
+  ));
+
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {movieList}
+    </div>
+  );
 }
 
 export default Movies;
